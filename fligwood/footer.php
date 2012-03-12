948eb718d0f86bc1e9e6b01d79850f12e3e55361
Justin Scott <fligtar@gmail.com>
2012-03-12 00:18:29 -0700

    removing gauges

--- fligwood/footer.php
+++ fligwood/footer.php
@@ -21,20 +21,5 @@
         document.documentElement.firstChild.appendChild(ga);
         })();
     </script>
-    
-    <!-- gauges -->
-    <script type="text/javascript">
-      var _gauges = _gauges || [];
-      (function() {
-        var t   = document.createElement('script');
-        t.type  = 'text/javascript';
-        t.async = true;
-        t.id    = 'gauges-tracker';
-        t.setAttribute('data-site-id', '4f532e94613f5d5c3700001e');
-        t.src = '//secure.gaug.es/track.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(t, s);
-      })();
-    </script>
 </body>
 </html>
